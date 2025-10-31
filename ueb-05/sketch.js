11eff465e16efd8f6f76d3c9c1c2de29e8c10fad
“SarahBonavia” <sarah.bonavia@stud.sfgz.ch>
2025-10-31 15:17:48 +0100

    TAG 2

--- ueb-05/sketch.js
+++ ueb-05/sketch.js
@@ -5,7 +5,6 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
   rectMode(CENTER);
-
 }
 
 
@@ -13,47 +12,58 @@ function draw() {
   background(0);
 
   push();
-  //Rechteck rechts
+  //Rechteck unten rechts
   //Koordinatesystem verschieben
-  translate(width/2 + 200, height/3);
+  translate(width/4 + 650, height/2);
   //rotieren
   rotate(drehwinkel);
 
-  fill(0, 0, 255);
-  rect (0, 0, 100, 100);
-
-  pop();
-
-
-  push();
-  //Rechteck links
-  //Koordinatesystem verschieben
-  translate(width/2 - 200, height/3);
-  //hier im Gegenuhrzeigersinn drehen
-  rotate(drehwinkel * -1); // suggestion von copilot
-
-  fill(0, 150, 255);
+  fill(100, 40, 255);
   rect (0, 0, 100, 100);
 
   pop();
 
     push();
-  //Rechteck rechts
+  //Rechteck unten zentrum
   //Koordinatesystem verschieben
-  translate(width/2 + 200, height/8);
+  translate(width/4 + 250, height/2);
   //rotieren
   rotate(drehwinkel);
 
-  fill(0, 0, 255);
+  fill(100, 255, 255);
+  rect (0, 0, 100, 100);
+
+  pop();
+
+  push();
+  //Rechteck unten links
+  //Koordinatesystem verschieben
+  translate(width/4 - 200, height/2);
+  //hier im Gegenuhrzeigersinn drehen
+  rotate(drehwinkel * -1); // suggestion von copilot
+
+  fill(255, 0, 255);
+  rect (0, 0, 100, 100);
+
+  pop();
+
+    push();
+  //Rechteck oben rechts
+  //Koordinatesystem verschieben
+  translate(width/2 + 150, height/8);
+  //rotieren
+  rotate(drehwinkel);
+
+  fill(150, 200, 255);
   rect (0, 0, 100, 100);
 
   pop();
 
 
   push();
-  //Rechteck links
+  //Rechteck oben links
   //Koordinatesystem verschieben
-  translate(width/2 - 200, height/8);
+  translate(width/2 - 300, height/8);
   //hier im Gegenuhrzeigersinn drehen
   rotate(drehwinkel * -1); // suggestion von copilot
 
